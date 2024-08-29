3513de41abb4f860e968799a1b21501d508e6974
THERohanPandey <rohanpandeey@gmail.com>
2024-08-29 15:44:15 +0530

    Header Section

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,17 +5,19 @@ import SingIn from "./pages/signIn";
 import Singup from "./pages/singup";
 import Dashboard from "./pages/Dashboard";
 import Projects from "./pages/Projects";
+import Header from "./components/Header";
 
 export default function App() {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/sing-in" element={<SingIn />} />
         <Route path="/sing-up" element={<Singup />} />
         <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/project" element={<Projects />} />
+        <Route path="/projects" element={<Projects />} />
       </Routes>
     </BrowserRouter>
   );
