5bb58b4c9b767b336210734818e63bedf0ac4eda
THERohanPandey <rohanpandeey@gmail.com>
2024-08-29 12:06:15 +0530

    react-router-dom + components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,3 +1,22 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import SingIn from "./pages/signIn";
+import Singup from "./pages/singup";
+import Dashboard from "./pages/Dashboard";
+import Projects from "./pages/Projects";
+
 export default function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/sing-in" element={<SingIn />} />
+        <Route path="/sing-up" element={<Singup />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/project" element={<Projects />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
---
+++ client/src/pages/singup.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const singup = () => {
+  return <div>singup</div>;
+};
+
+export default singup;
